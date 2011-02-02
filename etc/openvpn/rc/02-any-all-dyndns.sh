dacd41de6c1a006d9cd5300ee3172ff41942c778
ivandeex <ivandeex@gmail.com>
2011-02-02 14:30:17 +0000

    prepare framework for cron jobs

--- etc/openvpn/rc/02-any-all-dyndns.sh
+++ etc/openvpn/rc/02-any-all-dyndns.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # update dynamic dns zone
 # $Id$
+
+[ $RC_ACTION = up -o $RC_ACTION = down ] || exit 1
+
 if [ -z "$RC_CLIENT" -o -z "$RC_IP" -o -z "$NS_SERVER" -o -z "$VPN_ZONE" ]; then
     echo "dyndns: missing parameter: client:$RC_CLIENT ip:$RC_IP ns:$NS_SERVER zone:$VPN_ZONE"
     exit 1
 fi
+
 HOST=$RC_CLIENT.$VPN_ZONE
 (
     echo "server $NS_SERVER"
@@ -14,4 +18,5 @@ HOST=$RC_CLIENT.$VPN_ZONE
     echo "send"
 ) > $RC_TMP_VAR
 nsupdate $RC_TMP_VAR
+
 echo "dyndns: $RC_ACTION $HOST [$RC_IP] status:$?"
--- etc/openvpn/rc/03-any-all-iptables.sh
+++ etc/openvpn/rc/03-any-all-iptables.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-[ $RC_ACTION = up ] && CMD=I || CMD=D
+# $Id$
+
+case "$RC_ACTION" in
+  up)   CMD=I ;;
+  down) CMD=D ;;
+  *)    exit 1 ;;
+esac
+
 for CHAIN in $CHAINS; do
     iptables -$CMD $CHAIN -s $RC_IP -j ACCEPT
     echo "iptables: $RC_ACTION $CHAIN $RC_IP status:$?"
     iptables-save | egrep '^\-A '$CHAIN >> $RC_TMP_VAR
 done
+
 mv -f $RC_TMP_VAR $RC_VAR/iptables
