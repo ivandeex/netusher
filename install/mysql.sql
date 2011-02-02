9a9e7bbd01acd4378295b90cc7aab4f52658cd9a
ivandeex <ivandeex@gmail.com>
2011-02-02 18:44:39 +0000

    skeleton of traffic accounting tables

---
+++ install/mysql.sql
@@ -0,0 +1,22 @@
+-- $Id$
+-- Create database tables for UserWatch
+
+DROP DATABASE IF EXISTS userwatch;
+CREATE DATABASE userwatch DEFAULT CHARACTER SET utf8;
+USE userwatch;
+
+DROP TABLE IF EXISTS uw_traffic;
+CREATE TABLE uw_traffic (
+	cname		varchar(16) NOT NULL,
+	beg_time	datetime NOT NULL,
+	end_time	datetime,
+    running		tinyint(1),
+	real_ip		varchar(16),
+	real_port	int(5),
+	vpn_ip		varchar(16),
+	rx_bytes	int(8),
+	tx_bytes	int(8),
+	PRIMARY KEY (cname, beg_time)
+);
+
+
