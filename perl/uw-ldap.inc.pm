15728e2ba13ec19799849b3cfd3b7ae7016ac398
ivandeex <ivandeex@gmail.com>
2011-02-10 21:03:35 +0000

    replaces pipes with socket pair and secure against timeouts

--- perl/uw-ldap.inc.pm
+++ perl/uw-ldap.inc.pm
@@ -13,13 +13,16 @@ require "$Bin/uw-common.inc.pm";
 # require: perl-LDAP
 #
 use Net::LDAP;
+use Socket;
+use IO::Handle;
 
 our (%uw_config);
 
 my $ldap_maxtries = 2;
+my $ldap_try_delay = 0.100;
 
 our ($ldap_parent, $ldap_child);
-my  ($ldap_pid, $ldap_pipe_req, $ldap_pipe_reply);
+my  ($ldap_pid, $ldap_sock, $ldap_restart_count);
 my  ($ldap_conn, %uid_cache);
 
 #
@@ -55,7 +58,6 @@ sub ldap_close () {
 
     if ($ldap_parent) {
         _ldap_child_stop();
-        return;
     }
 
     # in ldap child or in a single process
@@ -75,9 +77,7 @@ sub _ldap_cache_flush () {
 sub ldap_auth ($$) {
     my ($user, $pass) = @_;
     if ($ldap_parent) {
-        print $ldap_pipe_req "AUTH $user $pass\n";
-        chomp(my $reply = <$ldap_pipe_reply>);
-        return $reply;
+        return _ldap_wait_reply("AUTH $user $pass");
     }
     # child or main process
     my ($msg, $conn) = _ldap_connect(undef, $user, $pass, 1);
@@ -120,8 +120,7 @@ sub _ldap_get_uid ($) {
     my ($uid, $msg);
     my $uid_attr = $uw_config{ldap_attr_user};
     if ($ldap_parent) {
-        print $ldap_pipe_req "UID $user\n";
-        chomp(my $reply = <$ldap_pipe_reply>);
+        my $reply = _ldap_wait_reply("UID $user");
         ($uid, $msg) = ($1, $2) if $reply =~ /^(\S+) (.*)$/;
         undef $uid if $uid eq "-";
     } else {
@@ -138,27 +137,32 @@ sub _ldap_get_uid ($) {
 sub _ldap_child_loop () {
     # child lives in the loop
     debug("ldap child waiting for commands");
-    while (<$ldap_pipe_req>) {
+    while (<$ldap_sock>) {
         chomp;
         my @cmd = split / /;
         debug("ldap child got command: %s", join(' ', @cmd));
-        if ($cmd[0] eq "PING") {
-            print $ldap_pipe_reply "OK\n";
-            next;
-        }
+        my ($quit, $reply);
         if ($cmd[0] eq "QUIT") {
-            last;
+            $reply = "BYE";
+            $quit = 1;
         }
-        if ($cmd[0] eq "UID") {
+        elsif ($cmd[0] eq "HELLO") {
+            #if ($ldap_restart_count < 3) { debug("child sleep"); sleep(1) for (0 .. 9); }
+            $reply = "OK";
+        }
+        elsif ($cmd[0] eq "UID") {
             my ($uid, $msg) = _ldap_get_uid($cmd[1]);
             $uid = "-" if !defined($uid) || $uid eq "";
-            print $ldap_pipe_reply "$uid $msg\n";
-            next;
+            $reply = "$uid $msg";
         }
-        if ($cmd[0] eq "AUTH") {
-            print $ldap_pipe_reply ldap_auth($cmd[1], $cmd[2]) . "\n";
-            next;
+        elsif ($cmd[0] eq "AUTH") {
+            $reply = ldap_auth($cmd[1], $cmd[2]);
         }
+        else {
+            $reply = "ERROR";
+        }
+        print $ldap_sock "$reply\n";
+        last if $quit;
     }
 }
 
@@ -168,41 +172,41 @@ sub _ldap_child_loop () {
 
 sub _ldap_child_start () {
     $SIG{PIPE} = "IGNORE";
-    $| = 1;
-    debug("forking off ldap child");
+    my ($sock_parent, $sock_child);
 
-    pipe(my ($pipe_req_r, $pipe_req_w)) or fail("pipe: $!");
-    $pipe_req_w->autoflush(1);
+    while (1) {
+        _ldap_child_stop(1);
+        $ldap_restart_count++;
+        debug("forking off ldap child $ldap_restart_count");
 
-    pipe(my ($pipe_reply_r, $pipe_reply_w)) or fail("pipe: $!");
-    $pipe_reply_w->autoflush(1);
+        socketpair($sock_parent, $sock_child, AF_UNIX, SOCK_STREAM, PF_UNSPEC)
+            or fail("socketpair: $!");
 
-    defined($ldap_pid = fork()) or fail("can't fork: $!");
+        defined($ldap_pid = fork()) or fail("can't fork: $!");
 
-    if ($ldap_pid) {
+        last unless $ldap_pid;
+
         # in the parent
         $ldap_parent = 1;
 
-        $ldap_pipe_req = $pipe_req_w;
-        close $pipe_req_r;
-        $ldap_pipe_reply = $pipe_reply_r;
-        close $pipe_reply_w;
-
+        close $sock_child;
+        $ldap_sock = $sock_parent;
+        $ldap_sock->autoflush(1);
+        
         debug("wait for status from ldap child");
-        print $ldap_pipe_req "PING\n";
-        chomp(my $reply = <$ldap_pipe_reply>);
+        my $reply = _ldap_wait_reply("HELLO", 1);
+        next unless $reply;
         debug("status from ldap child: $reply");
-        return;
+        return $reply;
     }
-    
+
     # in the child
     debug("ldap child starting");
     $ldap_child = 1;
 
-    $ldap_pipe_req = $pipe_req_r;
-    close $pipe_req_w;
-    $ldap_pipe_reply = $pipe_reply_w;
-    close $pipe_reply_r;
+    close $sock_parent;
+    $ldap_sock = $sock_child;
+    $ldap_sock->autoflush(1);
 
     # close ssl connections
     cleanup();
@@ -210,6 +214,7 @@ sub _ldap_child_start () {
 
     _ldap_init();
     _ldap_child_loop();
+
     debug("quitting ldap child");
     ldap_close();
     exit(0);
@@ -218,17 +223,68 @@ sub _ldap_child_start () {
 sub _ldap_child_stop ($) {
     my ($force) = @_;
 
-    print $ldap_pipe_req "QUIT\n";
-    waitpid $ldap_pid, 0;
-    debug("ldap child terminated");
+    if (defined $ldap_pid) {
+        if ($force) {
+            kill(9, $ldap_pid);
+        } elsif (!_ldap_wait_reply("QUIT", 1)) {
+            info("ldap child hung during exit");
+            kill(9, $ldap_child);
+        }
+        waitpid($ldap_pid, WNOHANG);
+        undef $ldap_pid;
+        debug("ldap child terminated");
+    }
 
-    close $ldap_pipe_req;
-    undef $ldap_pipe_req;
-    close $ldap_pipe_reply;
-    undef $ldap_pipe_reply;
+    if (defined $ldap_sock) {
+        shutdown $ldap_sock, 2;
+        close $ldap_sock;
+        undef $ldap_sock;
+    }
 
     $ldap_parent = $ldap_child = 0;
-    undef $ldap_pid;
+}
+
+#
+# exchange with child process
+#
+
+sub _ldap_send_req ($) {
+    my ($req) = @_;
+    debug("to ldap child: \"$req\"");
+    print $ldap_sock "$req\n";
+}
+
+sub _ldap_wait_reply ($$) {
+    my ($req, $norestart) = @_;
+    my $timeout = $uw_config{ldap_timeout}
+                + ($ldap_maxtries + 1) * $ldap_try_delay;
+
+    for (my $try = 1; $try <= $ldap_maxtries; $try++) {
+        _ldap_send_req($req) if defined $req;
+        my ($vec, $nfound);
+        my $begtime = time();
+        my $remaining = $timeout;
+        while ($remaining > 0) {
+            vec($vec, fileno($ldap_sock), 1) = 1;
+            debug("DEBUG: parent wait for %f seconds", $remaining);
+            ($nfound) = select($vec, undef, undef, $remaining);
+            last if $nfound > 0;
+            $remaining = $timeout - (time() - $begtime);
+        }
+        debug("DEBUG: parent got $nfound after wait: $!");
+        if ($nfound > 0) {
+            # read the reply
+            chomp(my $reply = <$ldap_sock>);
+            debug("from ldap child: \"%s\"", $reply);
+            return $reply if defined $reply;
+        }
+        last if $norestart;
+        info("ldap child hung");
+        _ldap_child_start(); # restart and try again
+    }
+
+    debug("nothing from ldap child");
+    return;
 }
 
 #
@@ -308,7 +364,7 @@ sub _ldap_search ($$) {
             undef $ldap_conn;
             _ldap_init();
             # a little delay
-            select(undef, undef, undef, ($try - 1) * 0.100);
+            select(undef, undef, undef, $ldap_try_delay);
         } else {
             last;
         }
