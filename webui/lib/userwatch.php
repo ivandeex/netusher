7a7793893c335d1dafe38894aa01b518a895c445
ivandeex <ivandeex@gmail.com>
2011-02-20 21:31:45 +0000

    user and host tables with sorting

--- webui/lib/userwatch.php
+++ webui/lib/userwatch.php
@@ -36,40 +36,109 @@ function setup () {
     return "";
 }
 
-/*
-    vpn_ip      varchar(16) NOT NULL,
-    beg_time    datetime NOT NULL,
-    end_time    datetime,
-    running     tinyint(1),
-    cname       varchar(16),
-    real_ip     varchar(16),
-    real_port   int(5),
-    rx_bytes    int(8),
-    tx_bytes    int(8),
-*/
-
-function show_hosts () {
-    echo "<h1>Hosts</h1>\n";
-    $count = 0;
-    $res = mysql_query("select count(*) from uw_openvpn");
-    if ($res) {
-        $row = mysql_fetch_array($res);
-        if ($row)
-            $count = $row[0];
+function setup_sort ($table, $columns) {
+    $sort_field = isset($_GET["$table-sort"]) ? $_GET["$table-sort"] : null;
+    $sort_dir = isset($_GET["$table-dir"]) ? $_GET["$table-dir"] : null;
+    if ($sort_dir != "asc" && $sort_dir != "desc") {
+        $sort_dir = null;
     }
-    echo "<p>$count host record(s)</p>\n";
+    if (!$sort_dir || !array_key_exists($sort_field, $columns)) {
+        $sort_dir = null;
+        $sort_field = null;
+    }
+    return array($sort_field, $sort_dir);
 }
 
-function show_users () {
-    echo "<h1>Users</h1>\n";
-    $count = 0;
-    $res = mysql_query("select count(*) from uw_users");
-    if ($res) {
-        $row = mysql_fetch_array($res);
-        if ($row)
-            $count = $row[0];
+function table_header ($self, $table, $columns) {
+    echo "<div class=\"clear-block\"></div>\n";
+    echo "<div class=\"$table-query query-results\">\n";
+    echo "<table class=\"$table-block index-block\">\n";
+    echo "<thead><tr>\n";
+    list($sort_field, $sort_dir) = setup_sort($table, $columns);
+    foreach ($columns as $field => $title) {
+        $sort = $sort_field === $field ? $sort_dir : "";
+        $rev_sort = $sort === "asc" ? "desc" : "asc";
+        echo "<th>";
+        echo "<a href=\"$self?$table-sort=$field&amp;$table-dir=$rev_sort";
+        foreach ($_GET as $param => $value) {
+            if ($param != "$table-sort" && $param != "$table-dir")
+                echo "&amp;$param=$value";
+        }
+        echo "\">$title</a>";
+        if (!empty($sort)) echo "<img alt=\"$sort\" title=\"$sort\" src=\"$sort.png\" />";
+        echo "</th>\n";
     }
-    echo "<p>$count user record(s)</p>\n";
+    echo "</tr></thead>\n";
+    echo "<tbody>\n";
+}
+
+function table_footer ($table, $count) {
+    echo "</tbody>\n";
+    echo "</table>\n";
+    echo "<p>$count $table record(s)</p>\n";
+    echo "</div>";
+}
+
+function table_query ($table, $db_table, $columns) {
+    $query = "select ";
+    $query .= implode(",", array_keys($columns));
+    $query .= " from $db_table ";
+    list($sort_field, $sort_dir) = setup_sort($table, $columns);
+    if ($sort_field && $sort_dir)  $query .= "order by $sort_field $sort_dir";
+    return $query;
+}
+
+function table_fetch_all ($query, $columns) {
+    $count = 0;
+    $res = mysql_query($query);
+    while ($row = mysql_fetch_array($res)) {
+        $count++;
+        $even_odd = $count & 1 ? "odd" : "even";
+        echo "<tr class=\"$even_odd\">";
+        foreach ($columns as $field => $title) {
+            $value = $row[$field];
+            echo "<td>$value</td>";
+        }
+        echo "</tr>\n";
+    }
+    return $count;
+}
+
+function show_hosts ($self) {
+    echo "<h1>Hosts</h1>\n";
+    $table = "host";
+    $columns = array(
+        "vpn_ip"    => "VPN IP",
+        "beg_time"  => "Start Time",
+        "end_time"  => "End Time",
+        "running"   => "Active",
+        "cname"     => "Common Name",
+        "real_ip"   => "Real IP",
+        "rx_bytes"  => "Received Bytes",
+        "tx_bytes"  => "Sent Bytes"
+        );
+    table_header($self, $table, $columns);
+    $query = table_query($table, "uw_openvpn", $columns);
+    $count = table_fetch_all($query, $columns);
+    table_footer($table, $count);
+}
+
+function show_users ($self) {
+    echo "<h1>Users</h1>\n";
+    $table = "user";
+    $columns = array(
+        "beg_time"  => "Start Time",
+        "end_time"  => "End Time",
+        "username"  => "Username",
+        "vpn_ip"    => "VPN IP",
+        "running"   => "Active",
+        "method"    => "Method",
+        "sid"       => "SID"
+        );
+    table_header($self, $table, $columns);
+    $query = table_query($table, "uw_users", $columns);
+    $count = table_fetch_all($query, $columns);
+    table_footer($table, $count);
 }
 
 function userwatch () {
@@ -80,8 +149,9 @@ function userwatch () {
         @mysql_close($dbh);
         return;
     }
-    show_hosts();
-    show_users();
+    $self = "index.php";
+    show_hosts($self);
+    show_users($self);
     mysql_close($dbh);
 }
 
